2c3614e2314b026a4ef13a30c160a37cb3a21a1b
Harshita Kashyap <124551399+Harshita8526@users.noreply.github.com>
2023-09-10 17:13:54 +0530

    String Rotation.java

---
+++ String Rotation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Rotation {
+    public static void main(String[] args) {
+        String s;
+        System.out.println("Enter first string ");
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextLine();
+        String r;
+        System.out.println("Enter second string ");
+        Scanner sc1 = new Scanner(System.in);
+        r = sc.nextLine();
+
+
+    if(s.equalsIgnoreCase(r))
+
+    {
+        System.out.println("Both are rotation of each other ");
+    }
+    else
+
+    {
+        System.out.println("Both are not rotation of each other");
+    }
+
+}
+
+
+}
